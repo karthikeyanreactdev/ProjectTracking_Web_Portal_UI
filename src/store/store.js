87b3ecf78a6a05a5a2402bedd5a35336a3518e60
Karthikeyan D <81141000+karthikeyanreactdev@users.noreply.github.com>
2021-03-29 22:22:06 +0530

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import thunk from 'redux-thunk'
 import {createStore, applyMiddleware} from 'redux'
 import projectReducer from '../store/reducer/projectReducer';
-// creating and store and passing reducer and thunk middleware as paramter
+// creating and store and passing reducer and thunk middleware as parameter
 let store=createStore(projectReducer, applyMiddleware(thunk));
 export default store
